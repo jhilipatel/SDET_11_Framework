a958af1bd64283156509d9e2f5d927c7b14b7ca0
soura <soura@LAPTOP-U499ULHF>
2021-03-15 12:59:20 +0530

    Final Framework

---
+++ src/test/java/com/comcast/ContactTest/ContactTest.java
@@ -0,0 +1,26 @@
+package com.comcast.ContactTest;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+
+	@Test
+	public void createconcacttest()
+	{
+		System.out.println("execute create contact");
+	}
+	
+
+	@Test
+	public void modifyconcacttest()
+	{
+		System.out.println("execute modify contact");
+	}
+	
+
+	@Test
+	public void deleteconcacttest()
+	{
+		System.out.println("execute delete contact");
+	}
+}
---
+++ src/test/java/com/comcast/createProduct/CreateProductTest.java
@@ -0,0 +1,19 @@
+package com.comcast.createProduct;
+
+import org.testng.annotations.Test;
+
+public class CreateProductTest 
+{
+
+   @Test
+   public void createContact()
+     {
+	System.out.println("execute createContact");
+     }
+
+   @Test
+   public void createContactwithMobile()
+     {
+	   System.out.println("execute createContactwithMobile");
+     }
+}
